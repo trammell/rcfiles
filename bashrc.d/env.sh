c1e93f47ccb0e46c4cb79ee54bc299e9c3defceb
John Trammell <johntrammell@gmail.com>
2012-01-09 10:57:20 -0600

    moved locale settings into locale.sh

--- bashrc.d/env.sh
+++ bashrc.d/env.sh
@@ -1,8 +1,5 @@
 # set up miscellaneous environment variables
 
-export LC_ALL="en_US.utf8"
-export LANG="en_US.UTF-8"
-
 export FIGNORE="CVS:~:.o:.svn:.egg-info:.pyc:.old"
 export GREP_OPTIONS="-I"
 export LESS=-x4
