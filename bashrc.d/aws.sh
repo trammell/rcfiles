0d23c8a842346fd9693b7b36a5f575b825f3d6f6
John Trammell <john.trammell@outsell.com>
2014-07-18 13:45:22 -0500

    keychain love

---
+++ bashrc.d/aws.sh
@@ -0,0 +1,6 @@
+
+# set up CLI completion, see
+# http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
+
+complete -C aws_completer aws
+
--- bashrc.d/keychain.sh
+++ bashrc.d/keychain.sh
@@ -1,4 +1,14 @@
-eval $(keychain --eval --quiet outsell_jtrammell outsell_jtrammell_git johntrammell)
+#!/bin/bash
 
-ssh-add ~/.ssh/outsell_jtrammell_git
+# see also:
+#   http://www.funtoo.org/Keychain
+#   http://www.cyberciti.biz/faq/ssh-passwordless-login-with-keychain-for-scripts/
+# Re-use ssh-agent and/or gpg-agent between logins
+
+eval $(/usr/bin/keychain --eval --quiet outsell_jtrammell outsell_jtrammell_git johntrammell)
+
+#eval $(/usr/bin/keychain --quiet --eval $HOME/.ssh/outsell_jtrammell)
+#ssh-add ~/.ssh/outsell_jtrammell_git
+#ssh-add ~/.ssh/outsell_jtrammell
+#ssh-add ~/.ssh/johntrammell
 
